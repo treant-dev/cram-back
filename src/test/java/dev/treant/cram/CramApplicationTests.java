f95f85d5eb528ad831760c385e54aeb235121c33
Konstantin Kostikov <kostikov.konstantin.a@gmail.com>
2025-07-14 23:56:36 +0200

    spring init added

---
+++ src/test/java/dev/treant/cram/CramApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.treant.cram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
